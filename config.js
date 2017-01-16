d27c84f21cc534f34777c88631616681bcfa9f14
macdja38 <jakeincanada@icloud.com>
2017-01-16 13:20:52 -0500

    move position configs into a separate file for easier configuring

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by root on 16/01/17.
+ */
+
+config = {
+  specialLocations: [
+    {xMin: -10, xMax: 10, yMin: -10, yMax: 10, zMin: -10, zMax: 10, text: "Hello, you reached spawn"},
+  ]
+};
