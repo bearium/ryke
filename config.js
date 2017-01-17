424c47999ea065e82e4820bf059dceb9da33301c
macdja38 <jakeincanada@icloud.com>
2017-01-16 21:56:23 -0500

    optomise popup and add a second location

--- config.js
+++ config.js
@@ -5,5 +5,6 @@
 config = {
   specialLocations: [
     {xMin: -10, xMax: 10, yMin: -10, yMax: 10, zMin: -10, zMax: 10, text: "Hello, you reached spawn"},
+    {xMin: -105, xMax: -75, yMin: -10, yMax: 20, zMin: 305, zMax: 330, text: "Statue of Gandhi"},
   ]
 };
