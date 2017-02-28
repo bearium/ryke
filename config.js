be3ff16081dbe0ff302abf538626c3000cd981ac
Logan MacGillivray <lucky-mac11@users.noreply.github.com>
2017-02-27 23:41:19 -0500

    Changed locations to be proper

--- config.js
+++ config.js
@@ -4,7 +4,8 @@
 
 config = {
   specialLocations: [
-    {xMin: -10, xMax: 10, yMin: -10, yMax: 10, zMin: -10, zMax: 10, text: "Hello, you reached spawn"},
-    {xMin: -105, xMax: -75, yMin: -10, yMax: 20, zMin: 305, zMax: 330, text: "Statue of Gandhi"},
+    {xMin: -60, xMax: -40, yMin: 40, yMax: 60, zMin: 140, zMax: 160, text: "Hello, you reached spawn"},
+    {xMin: -40, xMax: -15, yMin: -10, yMax: 20, zMin: 365, zMax: 390, text: "Statue of Gandhi"},
+    {xMin: -320, xMax: -300, yMin: -10, yMax: 40, zMin: 245, zMax: 265, text: "The Yelow Structure"},
   ]
 };
